08686ead46673bdc710a90c5f2d005d5ad459435
Timothy <Hill@Timothys-MacBook-Pro.local>
2016-10-06 10:55:33 +0100

    Refactoring preview building.

--- autocomplete/munge/mongo_import/entities/preview_builder/PreviewBuilder.py
+++ autocomplete/munge/mongo_import/entities/preview_builder/PreviewBuilder.py
@@ -1,125 +1,83 @@
+import xml.etree.ElementTree as ET
+
+
 class PreviewBuilder:
 
-    CLASS_TO_URI_MAP = {
+    tree = ET.parse('preview_builder/professions.rdf')
 
-        'Agent': 'http://www.europeana.eu/schemas/edm/Agent',
-        'Concept' : 'https://www.w3.org/2009/08/skos-reference/skos.html#Concept',
-        'Place' : 'http://www.europeana.eu/schemas/edm/Place'
-
-    }
+    PROFESSIONS = tree.getroot()
+    ns = {'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'skos':'http://www.w3.org/2004/02/skos/core#', 'xml':'http://www.w3.org/XML/1998/namespace'}
 
     def __init__(self):
         from pymongo import MongoClient
         from ContextClassHarvesters import ContextClassHarvester
         import yaml
-        self.lv = ContextClassHarvester.LANG_VALIDATOR
         self.mongoclient = MongoClient(ContextClassHarvester.MONGO_HOST, ContextClassHarvester.MONGO_PORT)
-        self.legacymongo = MongoClient(ContextClassHarvester.LEGACY_MONGO_HOST, ContextClassHarvester.MONGO_PORT)
         with open('preview_builder/fieldconfig.yml') as yml:
             self.field_config = yaml.load(yml)
 
-    def build_preview(self, entity_type, entity_id, entity_rows):
+    def build_preview(self, entity_type, entity_id, entity_rows, language):
         import json
         fields_to_build = self.field_config[entity_type]
-        retfields = {}
-        retfields['type'] = self.CLASS_TO_URI_MAP[entity_type]
+        preview_fields = {}
+        preview_fields['id'] = entity_id
+        preview_fields['type'] = entity_type
         for field in fields_to_build:
-            val = getattr(self, 'build_' + field)(entity_rows)
-            retfields[field] = val
-        multilingual_labels = self.line_up_languages(retfields)
-        multilingual_labels['id'] = entity_id
-        return json.dumps(multilingual_labels)
+            val = getattr(self, 'build_' + field)(entity_rows, language, preview_fields)
+        return json.dumps(preview_fields)
 
-    def build_prefLabel(self, entity_rows):
-        labels = {}
-        for lang in entity_rows['representation']['prefLabel']:
-            if(self.lv.pure_validate_lang_code(lang)):
-                labels[lang] = entity_rows['representation']['prefLabel'][lang]
-        return labels
+    def build_prefLabel(self, entity_rows, language, preview_fields):
+        lang_key = 'def' if language not in entity_rows['representation']['prefLabel'] else language
+        if lang_key in entity_rows['representation']['prefLabel']:
+            preview_fields['prefLabel'] = entity_rows['representation']['prefLabel'][lang_key][0]
 
-    def build_lifespan(self, entity_rows):
+    def build_lifespan(self, entity_rows, language, preview_fields):
         # TODO: Validation routines to ensure agents have only one birthdate and deathdate apiece
-        birthdates = {}
-        deathdates = {}
         if('rdaGr2DateOfBirth' in entity_rows['representation'].keys()):
-            for lang in entity_rows['representation']['rdaGr2DateOfBirth']:
-                birthdates[lang] = entity_rows['representation']['rdaGr2DateOfBirth'][lang][0]
+            lang_key = 'def' if language not in entity_rows['representation']['rdaGr2DateOfBirth'] else language
+            if lang_key in entity_rows['representation']['rdaGr2DateOfBirth']:
+                preview_fields['dateOfBirth'] = entity_rows['representation']['rdaGr2DateOfBirth'][lang_key][0]
         if('rdaGr2DateOfDeath' in entity_rows['representation'].keys()):
-            for lang in entity_rows['representation']['rdaGr2DateOfDeath']:
-                deathdates[lang] = entity_rows['representation']['rdaGr2DateOfDeath'][lang][0]
-        births_w_deaths = []
-        births_wo_deaths = []
-        deaths_wo_births = []
-        for k in birthdates.keys():
-            if k in deathdates.keys() or 'def' in deathdates.keys(): births_w_deaths.append(k)
-            else: births_wo_deaths.append(k)
-        for k in deathdates.keys():
-            if k not in birthdates.keys() and 'def' not in birthdates.keys():deaths_wo_births.append(k)
-        all_dates = {}
-        for lang in births_w_deaths:
-            birthdate = birthdates[lang]
-            deathdate = deathdates[lang] if lang in deathdates.keys() else deathdates['def']
-            all_dates[lang] = birthdate + "-" + deathdate
-        for lang in births_wo_deaths:
-            birthdate = birthdates[lang]
-            all_dates[lang] = birthdate + "-"
-        for lang in deaths_wo_births:
-            deathdate = deathdates[lang]
-            all_dates[lang] = "? -" + deathdate
-        return all_dates
+            lang_key = 'def' if language not in entity_rows['representation']['rdaGr2DateOfDeath'] else language
+            if lang_key in entity_rows['representation']['rdaGr2DateOfDeath']:
+                preview_fields['dateOfDeath'] = entity_rows['representation']['rdaGr2DateOfDeath'][lang_key][0]
 
-    def build_role(self, entity_rows):
-        roles = {}
+    def build_role(self, entity_rows, language, preview_fields):
         if('rdaGr2ProfessionOrOccupation' in entity_rows['representation'].keys()):
-            for lang in entity_rows['representation']['rdaGr2ProfessionOrOccupation']:
-                roles[lang] = entity_rows['representation']['rdaGr2ProfessionOrOccupation'][lang]
-        return roles
+            lang_key = 'def' if language not in entity_rows['representation']['rdaGr2ProfessionOrOccupation'] else language
+            if lang_key in entity_rows['representation']['rdaGr2ProfessionOrOccupation']:
+                roles = []
+                for role in entity_rows['representation']['rdaGr2ProfessionOrOccupation'][lang_key]:
+                    if role.startswith('http'):
+                        test_role = PreviewBuilder.PROFESSIONS.find('./rdf:Description[@rdf:about="' + role + '"]/skos:prefLabel[@xml:lang="' + language + '"]', PreviewBuilder.ns)
+                        if test_role is None:
+                            test_role = role.split("/")[-1]
+                        else:
+                            test_role = test_role.text
+                        roles.append(test_role)
+                    else:
+                        roles.append(role)
+                preview_fields['professionOrOccupation'] = roles
 
-    def build_country(self, entity_rows):
-        country = {}
-        if 'parent' in entity_rows.keys():
-            next_level_up = entity_rows['parent']
-            nlu_chunk = self.mongoclient.annocultor_db.TermList.find_one({'codeUri': next_level_up })
-            for lang in nlu_chunk['representation']['prefLabel']:
-                    country[lang] = nlu_chunk['representation']['prefLabel'][lang]
-        return country
+    def build_country(self, entity_rows, language, preview_fields):
+        if 'isPartOf' in entity_rows['representation'].keys():
+            parents = []
+            for lang_key in entity_rows['representation']['isPartOf']:
+                for parent_uri in entity_rows['representation']['isPartOf'][lang_key]:
+                    parent = self.mongoclient.annocultor_db.TermList.find_one({ 'codeUri' : parent_uri})
+                    if(parent is not None):
+                        pr_lang_key = lang_key if lang_key in parent['representation']['prefLabel'].keys() else ''
+                        label = parent['representation']['prefLabel'][pr_lang_key][0]
+                        pr_rep = { "id" : parent_uri, "prefLabel" : label }
+                        parents.append(pr_rep)
+            preview_fields['isPartOf'] = parents
 
-    def build_topConcept(self, entity_rows):
+    def build_topConcept(self, entity_rows, language):
+        # TODO: update this method once top concepts dereferenceable
         concepts = {}
-        # if 'broader' in entity_rows.keys():
-        #    category_uri = entity_rows['broader']
-        #    print(category_uri)
-        #    top_concept = self.legacymongo.Concept.find_one({'about' : category_uri })
-        #    for lang in top_concept['prefLabel'].keys():
-        #        label = top_concept['prefLabel'][lang][0]
-        #         concepts[lang] = label
         concepts['def'] = 'Concept'
         return concepts
 
 
-    def build_dateRange(self, entity_rows):
+    def build_dateRange(self, entity_rows, language):
         pass
-
-    def line_up_languages(self, langmap):
-        all_langs = {}
-        pref_labels = langmap['prefLabel']
-        for lang in pref_labels:
-            if(len(lang) == 0): continue
-            all_langs[lang] = {}
-            all_langs[lang]['prefLabel'] = pref_labels[lang]
-            all_langs[lang]['type'] = langmap['type']
-            for key in langmap:
-                if key == 'prefLabel': continue
-                if key == 'type': continue
-                else:
-                    if langmap[key] is None:
-                        all_langs[lang][key] = None
-                    elif lang in langmap[key]:
-                        try:
-                            # print("Lang is " + lang + " and key is " + key)
-                            all_langs[lang][key] = langmap[key][lang]
-                        except TypeError as te:
-                            print("ERROR!:"+ langmap)
-                    elif 'def' in langmap[key]:
-                        all_langs[lang][key] = langmap[key]['def']
-        return all_langs
