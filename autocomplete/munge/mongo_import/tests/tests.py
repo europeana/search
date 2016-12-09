21b68d2b3f7b86408947da711884cd482a620a15
Timothy <Hill@Timothys-MacBook-Pro.local>
2016-12-09 21:55:06 +0000

    Error output streamlined and tweaked.

--- autocomplete/munge/mongo_import/tests/tests.py
+++ autocomplete/munge/mongo_import/tests/tests.py
@@ -8,7 +8,7 @@
 #
 #=========================================================================#
 
-import requests, json, os, sys, re
+import requests, json, os, sys, re, time
 from pymongo import MongoClient
 import entities.ContextClassHarvesters
 import xml.etree.ElementTree as ET
@@ -35,7 +35,7 @@ class StatusReporter:
         self.entity_id = entity_id
         self.msg = msg
 
-    def display(suppress_stdout=False, log_to_file=False):
+    def display(self, suppress_stdout=False, log_to_file=False):
         msg = self.test_name + " "
         if(self.status == "OK"):
             msg += "PASSED"
@@ -45,6 +45,7 @@ class StatusReporter:
         if(not(suppress_stdout)): print(msg + "\n")
         if(log_to_file):
             filepath =  os.path.join(os.path.dirname(__file__), '..', 'logs', 'import_tests', 'transform.log')
+            msg = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ": " + msg
             with open(filepath, 'a') as l:
                 l.write(msg + "\n")
 
@@ -85,12 +86,12 @@ def test_transform():
     for test_entity in test_entities:
         ieb.build_individual_entity(test_entity, is_test=True)
     errors = test_files_against_mongo('dynamic')
-    errors.append(test_json_formation('dynamic'))
+    errors.extend(test_json_formation('dynamic'))
     return errors
 
 def test_against_reference():
     errors = test_files_against_mongo('reference')
-    errors.append(test_json_formation('reference'))
+    errors.extend(test_json_formation('reference'))
     return errors
 
 def test_files_against_mongo(filedir='reference'):
@@ -127,8 +128,10 @@ def test_files_against_mongo(filedir='reference'):
         missing_from_mongo = [xml_key for xml_key in from_xml.keys() if (trim_lang_tag(xml_key) in IFM.keys() and xml_to_mongo(xml_key) not in from_mongo.keys())]
         missing_from_xml = [mog_key for mog_key in from_mongo.keys() if (trim_lang_tag(mog_key) in FM.keys() and mongo_to_xml(mog_key) not in from_xml.keys())]
         if(len(missing_from_mongo) > 0 or len(missing_from_xml) > 0):
-            et = from_xml["internal_type"][0] + " " + from_xml["id"][0].split("/")[-1])
-            msg = ["Missing from mongo: " + str(missing_from_mongo)] ]
+            entity_type = from_xml["internal_type"][0]
+            idno = from_xml["id"][0]
+            et = entity_type + " " + str(idno.split("/")[-1])
+            msg = ["Missing from mongo: " + str(missing_from_mongo)]
             msg.append("Missing from XML: " + str(missing_from_xml))
             test_name = "Test " + filedir + " files against Mongo"
             msg = "; ".join(msg)
@@ -161,11 +164,10 @@ def test_mandatory_fields():
     # note that there are no mandatory fields defined
     # for individual contextual classes
     errors = []
-    mandatory_fields = ['id', 'internal_type', 'europeana_doc_count'
+    mandatory_fields = ['id', 'internal_type', 'europeana_doc_count',
                         'europeana_term_hits', 'wikipedia_clicks', 'suggest_filters',
                         'derived_score', 'skos_prefLabel', 'payload']
     total_docs = get_solr_total()
-
     for mfield in mandatory_fields:
         qry = SOLR_URI + mfield + ":*"
         try:
@@ -186,8 +188,7 @@ def output_discrepancy(solr_total, comparator_name, comparator_count, log=False)
     if (discrepancy == 0):
         return StatusReporter("OK", comparator_name + " Tally", "All Entities", "Solr and " + comparator_name + " counts tally")
     else:
-        msg = ["WARNING: Count mismatch between complete Solr count and " + comparator_name]
-        msg.append("Solr (all documents): " + str(solr_total))
+        msg = ["Count mismatch between complete Solr count and " + comparator_name]
         msg.append("Solr (all documents): " + str(solr_total))
         msg.append(comparator_name + ": " + str(comparator_count))
         msg.append("Discrepancy: " + str(discrepancy))
@@ -195,7 +196,7 @@ def output_discrepancy(solr_total, comparator_name, comparator_count, log=False)
         return StatusReporter("BAD", comparator_name + " Tally", "All Entities", msg)
 
 # test no newlines in output
-def test_json_formation(filedir='dynamic'):
+def test_json_formation(filedir):
     errors = []
     fullpath = os.path.join(os.path.dirname(__file__), 'testfiles', filedir)
     for filename in os.listdir(fullpath):
@@ -222,6 +223,7 @@ def test_json_formation(filedir='dynamic'):
             else:
                 text = text.replace('"', "'")
                 teststring = "{\"" + fieldname + "\":\"" + text + "\"}"
+            from_xml[fieldname] = text
             try:
                 as_json = json.loads(teststring)
             except ValueError as ve:
@@ -229,11 +231,11 @@ def test_json_formation(filedir='dynamic'):
                 errors.append(StatusReporter("BAD", "JSON validity test", entity_id, "Field " + fieldname + " with value " + text + " is not valid JSON"))
     return errors
 
-def run_test_suite():
+def run_test_suite(suppress_stdout=False, log_to_file=False):
     errors = []
-    errors.append(test_totals())
-    errors.append(test_transform())
-    errors.append(test_mandatory_fields())
-    errors.append(test_files_against_mongo())
-    for error in errors:
-        error.display()
+    errors.extend(test_totals())
+    errors.extend(test_transform())
+    errors.extend(test_against_reference())
+    errors.extend(test_mandatory_fields())
+    errors.extend(test_files_against_mongo())
+    for error in errors: error.display(suppress_stdout, log_to_file)
