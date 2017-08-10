5b5c455060baf07b4cb0fd7720867b37475643c6
Timothy <Hill@Timothys-MacBook-Pro.local>
2017-08-10 11:33:36 +0100

    Ensuring terms are logged per user/session rather than absolutely

--- log_munge/log_extractor/term_extractor.py
+++ log_munge/log_extractor/term_extractor.py
@@ -12,16 +12,18 @@ class TermExtractor:
 
 	def extract_terms(self):
 		for filename in os.listdir(self.entry_directory):
+			file_terms = []
 			with open(os.path.join(self.entry_directory, filename)) as inf:
 				for line in inf.readlines():
 					try:
 						(searchtype, _, _, searchterm, _, _) = line.split("\t")
 						if(searchtype == "SearchInteraction" and searchterm != "NO VALUE PROVIDED"):
-							self.terms.append(searchterm.strip())
+							file_terms.append(searchterm.strip())
 					except ValueError:
 						pass
+			self.terms.extend(set(file_terms))
 
 	def output_terms(self, outfile_name):
 		with open(os.path.join(self.terms_directory, outfile_name), 'w') as outf:
-			for term in sorted(set(self.terms)):
+			for term in sorted(self.terms):
 				outf.write(term + "\n")
