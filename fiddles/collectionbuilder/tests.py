255be79c3ec4881d919a55461a5fd560ca38d085
Timothy <Hill@Timothys-MacBook-Pro.local>
2017-08-28 22:38:09 +0200

    All XML-building methods tested and working

--- fiddles/collectionbuilder/tests.py
+++ fiddles/collectionbuilder/tests.py
@@ -1,117 +1,154 @@
 from django.test import SimpleTestCase
 from collectionbuilder.xmlutil import XMLQueryEditor
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
 class XMLQueryEditorTestCase(SimpleTestCase):
 
-	def setUp(self):
-		self.XQE = XMLQueryEditor.XMLQueryEditor()
 
 	def test_xml_loaded(self):
 		# XML is correctly loaded and parsed
-		root_node_name = False
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		root_node_name = xqe.get_tree().getroot().tag
 		self.assertEquals(root_node_name, 'query')
 
 	def test_generate_id(self):
 		# to ensure uniqueness, node ids are generated
 		# from timestamps
-		test_id = 0
-		timestamp = 1000
-		self.assertEquals(test_id, timestamp)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		test_time = datetime(2017, 8, 28, 10, 00, 00, 000) 
+		expected = "c02dd831a0333ce9e8b2c7259cd0d3f6"
+		actual = xqe.convert_timestamp_to_identifier(test_time)
+		self.assertEquals(expected, actual)
 
 	def test_retrieve_clause_by_id(self):
 		# given the id of a clause, we retrieve it correctly
-		value = False
-		self.assertEquals(value, 'l\'examen')
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		test_node = xqe.retrieve_node_by_id("1")
+		op = test_node.attrib["operator"]
+		field = test_node.find("field").text 
+		value = test_node.find("value").text
+		self.assertEquals(op, "FIRST")
+		self.assertEquals(field, "title")
+		self.assertEquals(value, "test title")
 
 	def test_retrieve_clause_group_by_id(self):
 		# given the id of a clause group, we retrieve it correctly
-		clause_count = 0
-		self.assertEquals(clause_count, 2)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		test_node = xqe.retrieve_node_by_id("2")
+		op = test_node.attrib["operator"]
+		clause_count = len(test_node.find("clause"))
+		self.assertEquals(op, "AND")
+		self.assertEquals(clause_count, 2)		
 
 	def test_remove_clause_by_id(self):
 		# the specified clause is removed from the XML tree
-		clause = False
-		self.assertIsNone(clause)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		prev_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		xqe.remove_node_by_id("1")
+		new_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		self.assertEquals(prev_number_of_clauses, new_number_of_clauses + 1)
+		self.assertIsNone(xqe.retrieve_node_by_id("1"))
 
 	def test_remove_clause_group_by_id(self):
 		# the specified clause group is removed from the XML tree
-		clause = False
-		self.assertIsNone(clause)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		prev_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause-group"))
+		xqe.remove_node_by_id("5")
+		new_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause-group"))
+		self.assertEquals(prev_number_of_clauses, new_number_of_clauses + 1)
+		self.assertIsNone(xqe.retrieve_node_by_id("5"))
 
 	def test_generate_clause(self):
 		# given an operator, a field, and a value, the correct
 		# XML clause is generated
-		expected = "<clause operator='or' deprecated='false' xml:lang='de'><field>subject</field><value>Quellenforschung</value></clause>"
-		actual = None
-		self.assertXMLEqual(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause = xqe.generate_clause(operator="OR", field="subject", value="Quellenforschung", lang="de")
+		self.assertEquals(clause.attrib["operator"], "OR")
+		self.assertEquals(clause.attrib["xml:lang"], "de")
+		self.assertEquals(clause.attrib["deprecated"], "false")
+		self.assertEquals(clause.attrib["negated"], "false")
+		self.assertEquals(clause.find("field").text, "subject")
+		self.assertEquals(clause.find("value").text, "Quellenforschung")
 
 	def test_generate_clause_group(self):
 		# given the material for two clauses and an operator,
 		# appropriately generates a new clause group
-		expected = "<clause-group operator='and' deprecated='false'><clause operator='or' deprecated='false' xml:lang='de'><field>subject</field><value>Quellenforschung</value></clause><clause operator='or' deprecated='false' xml:lang='en'><field>subject</field><value>Source criticism</value></clause></clause-group>"
-		actual = None
-		self.assertXMLEqual(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause_group = xqe.generate_clause_group(operator="FIRST")
+		self.assertEquals(clause_group.attrib["operator"], "FIRST")
+		self.assertEquals(clause_group.attrib["deprecated"], "false")
+		self.assertEquals(clause_group.attrib["negated"], "false")
+
+	def test_add_clause_at_root(self):
+		# a given clause is added to the XML tree
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		prev_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		clause = xqe.generate_clause(operator="OR", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clausular_element=clause)
+		new_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		self.assertEquals(prev_number_of_clauses + 1, new_number_of_clauses)
+		last_child = xqe.get_tree().getroot().findall(".//clause")[-1]
+		self.assertEquals(last_child.find("value").text, "Quellenforschung")
 
 	def test_add_clause(self):
 		# a given clause is added to the XML tree
-		clause = None 
-		self.assertIsNotNone(clause)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		prev_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		clause = xqe.generate_clause(operator="OR", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clausular_element=clause, to_el_id="2")
+		new_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause"))
+		self.assertEquals(prev_number_of_clauses + 1, new_number_of_clauses)
+		last_root_child = xqe.get_tree().getroot().findall("clause")[-1]
+		self.assertNotEquals(last_root_child.find("value").text, "Quellenforschung")
+		augmented_clause_group = xqe.retrieve_node_by_id("2")
+		last_child = augmented_clause_group.findall("clause")[-1]
+		self.assertEquals(last_child.find("value").text, "Quellenforschung")
 
 	def test_add_clause_group(self):
 		# a given clause group is added to the XML tree
-		clause_group = None
-		self.assertIsNotNone(clause_group)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause_group = xqe.generate_clause_group(operator="AND")
+		prev_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause-group"))
+		xqe.add_clausular_element(clausular_element=clause_group, to_el_id="5")
+		new_number_of_clauses = len(xqe.get_tree().getroot().findall(".//clause-group"))
+		added_group_children = len(xqe.retrieve_node_by_id("5").findall("clause-group")[-1].findall(".//*"))
+		self.assertEquals(prev_number_of_clauses + 1, new_number_of_clauses)
+		self.assertEquals(added_group_children, 0)
 
 	def test_deprecate_clause(self):
 		# the deprecation flag is correctly set and unset on clauses
-		is_deprecated = False 
-		self.assertTrue(is_deprecated)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.deprecate_by_id("1")
+		el_is_deprecated = xqe.retrieve_node_by_id("1").attrib["deprecated"]
+		self.assertEquals(el_is_deprecated, "true")		
 
-	def test_deprecate_clause_group(self):
-		# the deprecation flag is correctly set and unset on clause-groups
-		is_deprecated = False
-		self.assertTrue(is_deprecated)
+	def test_undeprecate_clause(self):
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.undeprecate_by_id("6")
+		el_is_deprecated = xqe.retrieve_node_by_id("6").attrib["deprecated"]
+		self.assertEquals(el_is_deprecated, "false")
 
 	def test_negate_clause(self):
 		# the negation (NOT) flag is correctly set and unset on clauses
-		is_deprecated = False 
-		self.assertTrue(is_deprecated)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.negate_by_id("1")
+		el_is_negated = xqe.retrieve_node_by_id("1").attrib["negated"]
+		self.assertEquals(el_is_negated, "true")	
 
-	def test_negate_clause_group(self):
-		# the negation (NOT) flag is correctly set and unset on clause-groups
-		is_deprecated = False
-		self.assertTrue(is_deprecated)
+	def test_unnegate_clause(self):
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.unnegate_by_id("3")
+		el_is_negated = xqe.retrieve_node_by_id("3").attrib["negated"]
+		self.assertEquals(el_is_negated, "false")	
 
 	def test_subsequent_nodes_inherit_not(self):
 		# any clause added to a group with a "not" clause
 		# already defined also has its flag set to "not"
-		expected = True
-		actual = None
-		self.assertEquals(expected, actual)
-
-	def test_add_clause_retains_order(self):
-		# adding a clause does not affect the ordering of existing nodes
-		expected_node_mark = "None"
-		found_node_mark = ""
-		self.assertEquals(expected_node_mark, found_node_mark)
-
-	def test_add_clause_group_retains_order(self):
-		# adding a clause group does not affect the ordering of existing nodes
-		expected_node_mark = "None"
-		found_node_mark = ""
-		self.assertEquals(expected_node_mark, found_node_mark)
-
-	def test_remove_clause_retains_order(self):
-		# removing a clause does not affect node ordering
-		expected_node_mark = "None"
-		found_node_mark = ""
-		self.assertEquals(expected_node_mark, found_node_mark)
-
-	def test_remove_clause_group_retains_order(self):
-		# removing a clause group does not affect node ordering
-		expected_node_mark = "None"
-		found_node_mark = ""
-		self.assertEquals(expected_node_mark, found_node_mark)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause = xqe.generate_clause(operator="OR", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clause, "2")
+		self.assertEquals(clause.attrib["negated"], "true")
 
 	# we need to ensure that the boolean operators (AND|OR) are 
 	# correctly handled when nodes are removed or added
@@ -119,49 +156,66 @@ class XMLQueryEditorTestCase(SimpleTestCase):
 	def test_first_node_operator_is_first(self):
 		# the first node in a group of siblings is
 		# without an operator and is labelled "FIRST"
-		expected = "FIRST"
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.remove_node_by_id("3")
+		xqe.remove_node_by_id("4")
+		xqe.remove_node_by_id("5")
+		clause = xqe.generate_clause(operator="OR", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clause, "2")
+		self.assertEquals(clause.attrib["operator"], "FIRST")		
 
 	def test_second_node_with_operator_unassigned_when_added(self):
 		# any node added immediately after the first nodes
 		# should be explicitly flagged as needing an operator
-		expected = "UNASSIGNED"
-		actual = None
-		self.assertEquals(expected, actual)
-
-	def test_second_node_shares_operator_with_following_siblings(self):
-		expected = "AND"
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.remove_node_by_id("4")
+		xqe.remove_node_by_id("5")
+		clause = xqe.generate_clause(operator="UNASSIGNED", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clause, "2")
+		# while we're at it, let's make sure this is being added to the
+		# right point in the tree
+		parent = xqe.retrieve_node_by_id("2")
+		pos = xqe.get_position(parent, clause)
+		self.assertEquals(pos, 2)
+		self.assertEquals(clause.attrib["operator"], "UNASSIGNED")
 
 	def test_subsequent_nodes_inherit_and(self):
 		# any clause added to a group with an "AND" clause
 		# already defined also has the "AND" operator
-		expected = "AND"
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause = xqe.generate_clause(operator="UNASSIGNED", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clause, "5")
+		self.assertEquals(clause.attrib["operator"], "AND")
 
 	def test_subsequent_nodes_inherit_or(self):
 		# any clause added to a group containing an "OR" clause
 		# also has the "OR" operator
-		expected = "OR"
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		clause = xqe.generate_clause(operator="UNASSIGNED", field="subject", value="Quellenforschung", lang="de")
+		xqe.add_clausular_element(clause, "2")
+		self.assertEquals(clause.attrib["operator"], "OR")
 
 	def test_remove_first_clause_changes_operator_on_second(self):
 		# when the second node becomes the first, its operator 
 		# should be "FIRST"
-		expected = "FIRST"
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.remove_node_by_id("1")
+		new_first_node = xqe.retrieve_node_by_id("2")
+		self.assertEquals(new_first_node.attrib["operator"], "FIRST")
 
 	def test_remove_first_clause_has_no_operator_effect_on_third(self):
 		# need to ensure that this effect does not spread too far
-		expected = ""
-		actual = None
-		self.assertEquals(expected, actual)
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		xqe.remove_node_by_id("3")
+		new_first_node = xqe.retrieve_node_by_id("5")
+		self.assertEquals(new_first_node.attrib["operator"], "OR")
 
+	def test_serialisation_to_query(self):
+		# the XML needs to be converted appropriately to query form
+		xqe = XMLQueryEditor.XMLQueryEditor()
+		serialised_query = xqe.serialise_to_solr_query()
+		expected = "title:\"test title\" AND (-proxy_dc_subject:\"test\" OR proxy_dc_subject:\"l'examen\" OR (CREATOR:\"Leonardo da Vinci\"))"
+		self.assertEquals(serialised_query, expected)
 
 
 
--- fiddles/collectionbuilder/xmlutil/XMLQueryEditor.py
+++ fiddles/collectionbuilder/xmlutil/XMLQueryEditor.py
@@ -1,8 +1,191 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime
+import hashlib
+import os
+import re
 
 class XMLQueryEditor:
 
-	XMLQuery = None
+	def __init__(self, query_name="test"):
+		self.query_name = query_name
+		self.build_tree()
 
-	def __init__(self):
-		pass
+	def build_tree(self):
+		self._tree = self.load_query_file()
+
+	def load_query_file(self):
+		filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'stored_queries', self.query_name + ".xml")
+		tree = ET.parse(filepath)
+		return tree
+
+	def get_tree(self):
+		return self._tree
+
+	def generate_identifier(self):
+		timestamp = datetime.now()
+		return self.convert_timestamp_to_identifier(timestamp)
+
+	def convert_timestamp_to_identifier(self, timestamp):
+		timestamp_as_string = timestamp.strftime("%Y%m%d%H%M%S%f")
+		hashed_timestamp = hashlib.md5(timestamp_as_string.encode('utf8')).hexdigest()
+		return hashed_timestamp
+
+	def retrieve_node_by_id(self, node_id):
+		node = self._tree.getroot().find(".//*[@node-id=\"" + node_id + "\"]")
+		return node
+
+	def remove_node_by_id(self, node_id):
+		parent = self._tree.getroot().find(".//*[@node-id=\"" + node_id + "\"]/..")
+		child = parent.find("*[@node-id=\"" + node_id + "\"]")
+		parent.remove(child)
+		for child in parent.findall("*"):
+			child.attrib["operator"] = self.determine_operator(parent, child)
+
+	def generate_clause(self, operator="UNASSIGNED", field="", value="", lang="en", deprecated=False, negated=False):
+		clause = ET.Element("clause")
+		clause.attrib["node-id"] = self.generate_identifier()
+		clause.attrib["operator"] = operator
+		clause.attrib["xml:lang"] = lang
+		clause.attrib["deprecated"] = str(deprecated).lower()
+		clause.attrib["negated"] = str(negated).lower()
+		field_el = ET.SubElement(clause, "field")
+		value_el = ET.SubElement(clause, "value")
+		field_el.text = field
+		value_el.text = value
+		return clause
+
+	def generate_clause_group(self, operator="UNASSIGNED", deprecated=False, negated=False):
+		clause_group = ET.Element("clause-group")
+		clause_group.attrib["operator"] = operator
+		clause_group.attrib["deprecated"] = str(deprecated).lower()
+		clause_group.attrib["negated"] = str(negated).lower()
+		return clause_group
+
+	def add_clausular_element(self, clausular_element, to_el_id=None):
+		to_clause = self._tree.getroot()
+		if(to_el_id is not None):
+			to_clause = self.retrieve_node_by_id(to_el_id)
+		clausular_element.attrib["negated"] = str(self.default_negation_setting(to_clause)).lower()
+		to_clause.append(clausular_element)
+		clausular_element.attrib["operator"] = self.determine_operator(to_clause, clausular_element)
+
+	def default_negation_setting(self, parent):
+		nots = ["not" for child in parent.findall("*") if child.attrib["negated"] == "true"]
+		return len(nots) > 0
+
+	def get_position(self, parent, child):
+		position = 1
+		for sibling in parent.findall("*"):
+			if(sibling is child):
+				break
+			else:
+				position += 1
+		return position
+
+	def get_effective_position(self, parent, child):
+		position = 1
+		for sibling in parent.findall("*"):
+			if(sibling is child):
+				break
+			elif(sibling.attrib["deprecated"] == "true"):
+				pass
+			else:
+				position += 1
+		return position
+
+	def determine_operator(self, parent, child):
+		position_in_sibs = self.get_position(parent, child)
+		if(position_in_sibs == 1):
+			return "FIRST"
+		if(child.attrib["operator"] != "UNASSIGNED"):
+			return child.attrib["operator"]
+		if(position_in_sibs == 2):
+			return "UNASSIGNED"
+		ands = ["and" for kid in parent.findall("*") if kid.attrib["operator"] == "AND"]
+		ors = ["or" for kid in parent.findall("*") if kid.attrib["operator"] == "OR"]
+		if(len(ands) > 0):
+			return "AND"
+		if(len(ors) > 0):
+			return "OR"
+		# this should probably be an error
+		return "UNASSIGNED"
+ 
+	def deprecate_by_id(self, node_id):
+		dep = self.retrieve_node_by_id(node_id)
+		try:
+			dep.attrib["deprecated"] = "true"
+		except KeyError:
+			return None
+
+	def undeprecate_by_id(self, node_id):
+		undep = self.retrieve_node_by_id(node_id)
+		try:
+			undep.attrib["deprecated"] = "false"
+		except KeyError:
+			return None
+
+	def negate_by_id(self, node_id):
+		neg = self.retrieve_node_by_id(node_id)
+		try:
+			neg.attrib["negated"] = "true"
+		except KeyError:
+			return None
+
+	def unnegate_by_id(self, node_id):
+		unneg = self.retrieve_node_by_id(node_id)
+		try:
+			unneg.attrib["negated"] = "false"
+		except KeyError:
+			return None
+
+	def serialise_to_solr_query(self, xml_struct=None):
+		query_string = ""
+		if(xml_struct is None):
+			xml_struct = self._tree
+		for child in xml_struct.findall("*"):
+			tag = child.tag
+			if(tag == "clause"):
+				query_string += self.serialise_clause_to_solr_query(xml_struct, child)
+			elif(child.attrib["deprecated"] == "false"): # if it's a clause group ....
+				operator = self.construct_operator(xml_struct, child)
+				negator = self.construct_negator(child)
+				clausular_contents = self.serialise_to_solr_query(child)
+				if(clausular_contents == ""):
+					query_string = ""
+				else:
+					query_string += " " + operator + " " + negator + "(" + clausular_contents.strip() + ")"	
+		query_string = re.sub(r'\s+', ' ', query_string)
+		if(not(query_string.startswith(" AND") and not(query_string.startswith(" OR")))):
+			query_string = query_string.lstrip()
+		return query_string
+
+	def serialise_clause_to_solr_query(self, parent, el):
+		if(el.attrib["deprecated"] == "true"): return ""
+		operator = self.construct_operator(parent, el)
+		negator = self.construct_negator(el)
+		field = el.find("field").text
+		value = el.find("value").text
+		clause_as_string = (" " + operator + " " + negator + field + ":\"" + value + "\"")
+		return clause_as_string
+
+	def construct_operator(self, parent, el):
+		pos = self.get_effective_position(parent, el)
+		op = el.attrib["operator"]
+		if(pos == 1):
+			return ""
+		if(op == "AND"):
+			return "AND"
+		if(op == "OR"):
+			return "OR"
+		return ""
+
+	def construct_negator(self, el):
+		neg = el.attrib["negated"]
+		if(neg == "true"):
+			return "-"
+		else:
+			return ""
+
+
+
+
