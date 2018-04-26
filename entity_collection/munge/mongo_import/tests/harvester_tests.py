fa4fc03c7dea12c5e15efda2be3ec5de9177ee77
gsergiu <sergiu.gordea@ait.ac.at>
2018-04-26 17:39:53 +0200

    #EA-934 hello world test using unittest

---
+++ entity_collection/munge/mongo_import/tests/harvester_tests.py
@@ -0,0 +1,30 @@
+# ========================================================================#
+#
+# Tests to ensure import process has run correctly.
+#
+# Note the scope of these tests: they test only that the import process
+# is yielding the expected output. They do nothing to validate the
+# data of the imported entities itself.
+#
+#=========================================================================#
+
+import unittest
+
+class HarvesterTest(unittest.TestCase):
+   
+    # tests on a couple of entities of each type
+    def test_transform(self):
+        #ieb = entities.ContextClassHarvesters.IndividualEntityBuilder()
+        test_entities = [
+             "http://data.europeana.eu/agent/base/11241",   # Paris Hilton
+             "http://data.europeana.eu/agent/base/146741",  # Leonardo da Vinci
+             "http://data.europeana.eu/place/base/40361",   # Den Haag
+             "http://data.europeana.eu/place/base/143914",  # Ferrara
+             "http://data.europeana.eu/concept/base/214",   # Neoclassicism
+             "http://data.europeana.eu/concept/base/207"    # Byzantine art
+        ]
+        for test_entity in test_entities:
+            #ieb.build_individual_entity(test_entity, is_test=True)
+            print("Entity uri: " + test_entity)
+
+
