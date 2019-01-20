a79c88a6f81b0fd646b8d15ca7eb2487c1190ad6
Monica Marrero <monica.marrero@europeana.eu>
2019-01-20 20:57:26 +0100

    Added utils/java/EuropeanaUtils

---
+++ util/java/EuropeanaUtils/src/europeana/utils/ExceptionThreadFactory.java
@@ -0,0 +1,24 @@
+package europeana.utils;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class ExceptionThreadFactory implements ThreadFactory {
+	
+	private String threadName = null;
+	
+	public void setName(String threadName) {
+		this.threadName = threadName;
+	}
+
+	@Override
+	public Thread newThread(Runnable r) {
+	    Thread thread = Executors.defaultThreadFactory().newThread(r);
+	    thread.setUncaughtExceptionHandler(new ThreadUncaughException());
+	    if (threadName != null) {
+	    	thread.setName(threadName);
+	    }
+	    return thread;
+	}
+
+}
