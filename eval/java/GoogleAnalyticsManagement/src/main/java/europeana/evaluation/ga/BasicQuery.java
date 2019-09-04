70df3b96bb48c2c9cfd1844b71767cd02007e997
Monica Marrero <monica.marrero@europeana.eu>
2019-09-04 15:40:20 +0200

    Google Analytics Management

---
+++ eval/java/GoogleAnalyticsManagement/src/main/java/europeana/evaluation/ga/BasicQuery.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+
+public class BasicQuery {
+	String query;
+	String language;
+	String collection;
+	Integer uniqueSearches;
+
+
+	
+	public String getQuery() {
+		return query;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public String getPortal() {
+		return collection;
+	}
+	public Integer getUniquePageViews() {
+		return uniqueSearches;
+	}
+
+	public BasicQuery(String query, String language, String collection, Integer uniqueSearches) {
+		this.query = query.replace("\"", "").replaceAll("“",  "").trim();
+		this.language = language;
+		this.collection = collection;
+		this.uniqueSearches = uniqueSearches;
+	}
+	
+	public static BasicQuery CreateQuery(String query, String language, String collection, Integer uniqueSearches) {
+		return new BasicQuery(query, language, collection, uniqueSearches);
+	}
+	
+	public static BasicQuery CreateQuery(String query, String language, String collection, Integer uniqueSearches, SolrClient solrClient)  throws SolrServerException, IOException {
+		return new BasicQuery(query, language, collection, uniqueSearches);
+	}
+	
+
+	public void Print(PrintWriter pw) {
+		pw.print(this.getQuery());
+		pw.print("\t");
+		pw.print(this.getPortal());
+		pw.print("\t");
+		pw.print(this.getLanguage());
+		pw.println();
+	}
+	
+}
--- eval/java/GoogleAnalyticsManagement/src/main/java/europeana/evaluation/ga/Process.java
+++ eval/java/GoogleAnalyticsManagement/src/main/java/europeana/evaluation/ga/Process.java
@@ -1,4 +1,3 @@
-package europeana.evaluation.ga;
 
 
 import java.io.FileOutputStream;
@@ -51,160 +50,21 @@ import europeana.utils.Utils;
  * 
  */
 
-public class Process {
+public class App {
 	
 	static Logger logger = Logger.getLogger(Process.class);
 	
-	//private static final String URL_PATTERN = "/portal/(?<lang>.*?)/(search|collections/(?<cname>.+?))\\?.*?q\\=(?<query>.*?)(&|$)";
-	private static final String URL_PATTERN = "/portal/(?<lang>[^/]+)(/collections/(?<cname>.+?)\\?)?";
-	private static final Pattern pattern = Pattern.compile(URL_PATTERN);
-	
-	private static List<Query> GetQueries(GetReportsResponse response) throws Exception {
-		List<Query> queries = new ArrayList<Query>();
-		
-		for (Report report: response.getReports()) {
-			ColumnHeader header = report.getColumnHeader();
-			List<String> dimensionHeaders = header.getDimensions();
-			int indexPage = dimensionHeaders.indexOf("ga:searchStartPage");
-			int indexTerm = dimensionHeaders.indexOf("ga:searchKeyword");
-			//List<MetricHeaderEntry> metricHeaders = header.getMetricHeader().getMetricHeaderEntries();
-			List<ReportRow> rows = report.getData().getRows();
-			
-			for (ReportRow row: rows) {
-				List<String> dimensions = row.getDimensions();
-				List<DateRangeValues> metrics = row.getMetrics();
-				String dim = dimensions.get(indexPage);
-				String query = dimensions.get(indexTerm);
-				Matcher matcher = pattern.matcher(dim);
-				if (matcher.find()) {
-					String lang = matcher.group("lang");
-					String cname = matcher.group("cname");
-					if (cname == null) {
-						cname = "portal";
-					}
-					//String query = matcher.group("query");
-					Integer uniqueViews = Integer.parseInt(metrics.get(0).getValues().get(0));
-					queries.add(new Query(query, lang, cname, uniqueViews));
-				} else {
-					System.out.println("Not processed ga:searchStartPage = " + dim);
-					//throw new IllegalArgumentException("Regex didn't recognize dimension " + report.getColumnHeader().getDimensions().get(indexPage) + ": " + dim);
-				}
-			}
-		}
-		return queries;
-	}
-	
-	private static List<Query> Sample(List<Query> queries, Integer sampleSize){
-		Collections.shuffle(queries);
-		return queries.subList(0, sampleSize);
-	}
 
-	private static List<Query> Filter(List<Query> queries){
-		return queries.stream().filter(p -> (p.uniqueSearches <= 10 && p.query.matches("[\"\'\\“A-Za-z\\s]+") && !p.query.contains("AND") && !p.query.contains("OR") && p.query.length() > 2)).collect(Collectors.toList());
-	}
-	
-	private static void AddEntityInfo(List<Query> queries, String solrClient) throws SolrServerException, IOException {
-		SolrClient client = new HttpSolrClient.Builder(solrClient).build();
-		for (Query q: queries) {
-			String queryLC = q.getQuery().toLowerCase();
-			SolrQuery query = new SolrQuery();
-			String solr_query = "skos_prefLabel:\"" + queryLC + "\" OR label:\"" + queryLC + "\" OR text:\"" + queryLC + "\"";
-			query.setQuery(solr_query);
-			QueryResponse response = SolrErrorHandling.Query(client, query);
-			for (SolrDocument doc: response.getResults()) {
-				Set<String> labels = new HashSet<String>();
-				Set<String> text = new HashSet<String>();
-				String type = doc.getFieldValue("type").toString();
-				labels.add(doc.getFieldValue("skos_prefLabel").toString().toLowerCase());
-				doc.getFieldValues("label").forEach(p -> labels.add(p.toString().toLowerCase()));
-				doc.getFieldValues("text").forEach(p -> text.add(p.toString().toLowerCase()));
-				if (labels.contains(queryLC)) {
-					q.AddEntityClassLabel(type);
-				}
-				if (text.contains(queryLC)) {
-					q.AddEntityClassText(type);
-				}
-				
-				String[] keywords = queryLC.split(" ");
-				Set<String> labelParts = new HashSet<String>(Arrays.asList(labels.toString().split("[\\W_]")));
-				Set<String> textParts = new HashSet<String>(Arrays.asList(text.toString().split("[\\W_]")));
-				boolean wholeEntity = true;
-				for (String item : keywords) {
-					if (!labelParts.contains(item)) {
-						wholeEntity = false;
-						break;
-					}
-				}
-				if (wholeEntity) {
-					q.AddEntityClassRelaxedLabel(type);
-				}
-				wholeEntity = true;
-				for (String item : keywords) {
-					if (!textParts.contains(item)) {
-						wholeEntity = false;
-						break;
-					}
-				}
-				if (wholeEntity) {
-					q.AddEntityClassRelaxedText(type);
-				}
-			}
-		}
-	}
-	
-	private static GetReportsResponse GoogleAnalyticsRequest(Path gakey, String startDate, String endDate, String gaSamplingLevel) throws GeneralSecurityException, IOException {
-		// Create the DateRange object.
-		DateRange dateRange = new DateRange();
-		dateRange.setStartDate(startDate);
-		dateRange.setEndDate(endDate);
-
-		// Create the Metrics object.
-		Metric searches = new Metric().setExpression("ga:searchUniques");
-
-		// Create the dimension object.
-		Dimension sterms = new Dimension().setName("ga:searchKeyword");
-		Dimension startPage = new Dimension().setName("ga:searchStartPage");
-
-		// Create the ReportRequest object.
-		ReportRequest request = new ReportRequest()
-				.setDateRanges(Arrays.asList(dateRange))
-				.setMetrics(Arrays.asList(searches))
-				.setFiltersExpression("ga:searchStartPage!@(entrance);ga:searchStartPage!@(other)")
-				//.setOrderBys(Arrays.asList(order))
-				.setSamplingLevel(gaSamplingLevel)
-				.setDimensions(Arrays.asList(sterms, startPage));
-				//.setPageSize(10000);
-	
-		europeana.utils.GoogleAnalytics ga = new GoogleAnalytics(gakey);
-		return ga.GetRequest(request);
-	}
-	
-	private static void Print(OutputStream os, List<Query> queries) {
-		PrintWriter pw = new PrintWriter(os);
-		queries.forEach(p -> {
-			pw.print(p.query);
-			pw.print("\t");
-			pw.print(p.collection);
-			pw.print("\t");
-			pw.print(p.language);
-			pw.print("\t");
-			pw.print(p.eLabel.toString());
-			pw.print("\t");
-			pw.print(p.eText.toString());
-			pw.print("\t");
-			pw.print(p.eRelaxedLabel.toString());
-			pw.print("\t");
-			pw.print(p.eRelaxedText.toString());
-			pw.println();
-		});
-		pw.close();
-	}
 	
 	public static void main(String[] args) throws Exception {
 		
 		System.out.println("Processing...");
-		InputStream is = Process.class.getClassLoader().getResourceAsStream("resources/log4j.properties");
-		PropertyConfigurator.configure(is);
+		
+		
+		
+		//InputStream is = Process.class.getClassLoader().getResourceAsStream("resources/log4j.properties");
+		//PropertyConfigurator.configure(is);
+		PropertyConfigurator.configure("log4j.properties");
 		Properties prop;
 		prop = Utils.readProperties(Process.class.getClassLoader().getResourceAsStream("resources/config.properties"));
 		String startDate = prop.getProperty("start_date");
@@ -213,18 +73,39 @@ public class Process {
 		Integer sampleSize = Integer.parseInt(prop.getProperty("sample_size"));
 		String gaSamplingLevel = prop.getProperty("ga_sampling");
 		Path gakey = Paths.get(prop.getProperty("gakey"));
+		SolrClient client = new HttpSolrClient.Builder(solrClient).build();
 
-
-		GetReportsResponse gaResponse = GoogleAnalyticsRequest(gakey, startDate, endDate, gaSamplingLevel);
-		List<Query> queries = GetQueries(gaResponse);
-		queries = Filter(queries);
-		queries = Sample(queries, sampleSize);
+		QueryList<EntityQuery> entityList = new QueryList<EntityQuery>(gakey, startDate, endDate, gaSamplingLevel, client);
+		entityList.Filter_SimpleQueries();
+		entityList.Filter_UniquePageViews(10);
+		entityList.Sample(sampleSize);
 		
-		AddEntityInfo(queries, solrClient);
-		OutputStream file = new FileOutputStream("query_sample.csv"); 
-		Print(file, queries);
-		file.close();
-		System.out.println("Done. Created file query_sample.csv");
+		OutputStream os_entity = new FileOutputStream("query_sample_entity.csv"); 
+		entityList.Print(os_entity);
+		os_entity.close();
+		System.out.println("Done. Created file query_sample_entity.csv");
+		
+		
+		QueryList<BasicQuery> basicList = new QueryList<BasicQuery>(gakey, startDate, endDate, gaSamplingLevel, client);
+		basicList.Filter_SimpleQueries();
+		Set<String> languages = new HashSet<String>();
+		basicList.getQueries().forEach(p -> languages.add(p.getLanguage()));
+		Integer sample = sampleSize / languages.size();
+		List<BasicQuery> listQueries = new ArrayList<BasicQuery>();
+		for (String langTag: languages) {
+			QueryList<BasicQuery> langQuery = new QueryList<BasicQuery>(basicList.getQueries());
+			langQuery.Filter_Language(langTag);
+			if (sample < langQuery.getQueries().size())
+				langQuery.Sample(sample);
+			listQueries.addAll(langQuery.getQueries());
+		}
+		QueryList<BasicQuery> langQueries = new QueryList<BasicQuery>(listQueries);
+		OutputStream os_lang = new FileOutputStream("query_sample_lang.csv"); 
+		langQueries.Print(os_lang);
+		os_lang.close();
+		System.out.println("Done. Created file query_sample_lang.csv.  Number of languages: " + languages.size() );
+
+
 	}
 	
 
