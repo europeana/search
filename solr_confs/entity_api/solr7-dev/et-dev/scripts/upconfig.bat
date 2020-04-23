02a5af7a5dfa45d99e21221cfbd1fe0123183aaa
gsergiu <sergiu.gordea@ait.ac.at>
2020-04-23 12:56:54 +0200

    #EA-1981 script to load configuration

---
+++ solr_confs/entity_api/solr7-dev/et-dev/scripts/upconfig.bat
@@ -0,0 +1,2 @@
+set SDIR=./
+java -classpath .;%SDIR%/server/lib/ext/*;%SDIR%/server/solr-webapp/webapp/WEB-INF/lib/* org.apache.solr.cloud.ZkCLI -cmd upconfig -zkhost localhost:9983 --confdir ../et-up/conf --confname entity-test
